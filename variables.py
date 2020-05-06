08ded28a00aeb88633ed07b6d522c540addd5a68
alexbednarek4 <alexbednarek4@gmail.com>
2020-05-06 16:21:29 -0400

    initial commit

---
+++ variables.py
@@ -0,0 +1,16 @@
+"""
+Variables are case sensitive
+Can start with letter or underscore
+Can have lettors and numbers
+"""
+
+x, y, is_cool, name = (2, 4.5, True, "Alex")
+#print(x, y, is_cool, name)
+#checking type
+#print(type(x))
+
+#casting 
+x = str(x)
+y = int(y)
+z = float(y)
+print(type(z), z)
