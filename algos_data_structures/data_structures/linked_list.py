a75b0cd5ae2e185c18ad6ea2466cf397d3ae609a
alexbednarek4 <alexbednarek4@gmail.com>
2020-05-26 13:23:04 -0400

    Diameter of BST leetcode

---
+++ algos_data_structures/data_structures/linked_list.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert(self, node):
+
+    def contains(self, value):
+
+    def print_list(self):
+
+    
--- algos_data_structures/data_structures/max_heap.py
+++ algos_data_structures/data_structures/max_heap.py
@@ -19,26 +19,65 @@ right(i) = i * 2 + 1 = 9
 
 class MaxHeap():
     def __init__(self, items = []):
-        super()
-
-    def bubble_up(self, value):
-    
-    def bubble_down(self, value):
-
+        super().__init__()
+        # We start our elements at index 1 in a max heap
+        self.heap = [0]
+        for item in items:
+            self.heap.append(item)
+            self.__floatUp(len(self.heap) - 1)
     # Push
         # Add value to end of list
         # Float value up to proper position
-    def push(self, value):
+    def push(self, data):
+        self.heap.append(data)
+        self.__floatUp(len(self.heap) - 1)
     
     # Pop
         # Move max to end of list
         # Delete it
         # Bubble down item at index 1 to proper position
         # Return max
-    def pop():
+    def pop(self):
+        if len(self.heap) > 2:
+            self.__swap(1, len(self.heap) - 1)
+            max = self.heap.pop()
+            self.__bubbleDown(1)
+        elif len(self.heap) == 2:
+            max = self.heap.pop()
+        else:
+            max = False
+        return max
 
     # return value at top of heap
     def top(self):
+        if self.heap[1]:
+            return self.heap[1]
+        else:
+            return False
 
-
+    def __swap(self, i, j):
+        self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
     
+    def __floatUp(self, index):
+        parent = index//2
+        if index <= 1:
+            return
+        elif self.heap[index] > self.heap[parent]:
+            self.__swap(index, parent)
+            self.__floatUp(parent)
+    
+    def __bubbleDown(self, index):
+        # Left and right children
+        left = index * 2
+        right = index * 2 + 1
+        largest = index
+        if len(self.heap) > left and self.heap[largest] < self.heap[left]:
+            largest = left
+        if len(self.heap) > right and self.heap[largest] < self.heap[right]:
+            largest = right
+        if largest != index:
+            self.__swap(index, largest)
+            self.__bubbleDown(largest)
+
+    def __str__(self):
+        return str(self.heap)
